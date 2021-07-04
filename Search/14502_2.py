1958876bad640147b6c5b0315bc4d479562d0406
yoondabin <yundabin0608@gmail.com>
2021-07-04 21:13:27 +0900

    -

--- Search/14502_2.py
+++ Search/14502_2.py
@@ -3,23 +3,8 @@ from itertools import combinations
 import copy
 sys.setrecursionlimit(100000)
 
-empty_index=[]
-virus_index=[]
-
-N, M = map(int, input().split())
-matrix_in=[]
-for i in range(N):
-    matrix_in.append(list(map(int, input().split())))
-    for j in range(M):
-        if matrix_in[i][j]==0:
-            empty_index.append([i,j])
-        if matrix_in[i][j]==2:
-            virus_index.append([i,j])
-
-check = [[0]*M for _ in range(N)]
-
 # DFS 바이러스가 퍼지는 것을 구현
-def dfs(matrix, ck, x, y):
+def dfs(matrix, ck, x, y): #주어진 matrix, check행렬, x, y(바이러스 matrix의 좌표들)
     if ck[x][y] == 0:
         ck[x][y] = 1
         if matrix[x][y] == 0:
@@ -35,14 +20,31 @@ def dfs(matrix, ck, x, y):
             dfs(matrix, ck, x, y+1)
     return matrix
 
+empty_index=[] # empty는 벽을 임의로 세울 combination 결정할때 씀
+virus_index=[] 
+matrix_in=[]
 result = 0
-for coms in combinations(empty_index, 3):
+
+N, M = map(int, input().split())
+
+for i in range(N):
+    matrix_in.append(list(map(int, input().split())))
+    for j in range(M):
+        if matrix_in[i][j]==0:
+            empty_index.append([i,j])
+        if matrix_in[i][j]==2:
+            virus_index.append([i,j])
+
+check = [[0]*M for _ in range(N)] # 바이러스가 이미 퍼졌는지 아닌지 체크하는 용도
+# empty_index에서 빈칸중 벽 세울 임의의 3자리 고르는것
+for wall in combinations(empty_index, 3):
  
+    # 2차원 이상의 다차원 리스트 완전 복사는 copy 모듈의 deepcopy()함수 이용
     V_in = copy.deepcopy(matrix_in)
     cks = copy.deepcopy(check)
     
     # 벽세우기
-    for i, j in coms:
+    for i, j in wall:
         V_in[i][j] = 1
 
     # 바이러스 퍼뜨리기
