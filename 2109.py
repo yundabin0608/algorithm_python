666e55c69d5fff47facf97fb59b7e76c81cca23f
yoondabin <yundabin0608@gmail.com>
2022-03-06 22:36:45 +0900

    순회강연 GOLD3  heapq. (fix 정렬필요X)

--- 2109.py
+++ 2109.py
@@ -1,4 +1,4 @@
-# 순화강연
+# 순회강연
 
 # N개 대학에서 강연 요청, d일 안에 와서 강연하면 q의 강의료 지불 (N,d,q: 1~10000)
 # 학자는 하루에 최대 한곳에서 강연. 최대로 벌 수 있는 돈 출력
@@ -11,7 +11,7 @@ for i in range(N):
     lectures.append(list(map(int, input().split())))
 
 # 리스트 둘째 인자를 오름차순으로 먼저 정렬 후 첫째 인자를 내림차순으로 정렬
-lectures.sort(key=lambda x: (x[1], -x[0]))
+# lectures.sort(key=lambda x: (x[1], -x[0])) 안해도됨
 tmp=[] 
 
 for q,d in lectures:
@@ -31,19 +31,19 @@ print(sum(tmp))
 # print(sum(tmp))
 
 # PAY 의 정렬 유무
-# 5
+5
 # 2 1
 # 20 1
 # 40 1
 # 2 2
 # 8 2
 
-# (pay 정렬 X)          (pay 정렬 O)
-# push [2]             push [40]
-# push [2, 20]         push [20, 40]
-# ***pop*** [20]       ***pop*** [40]
-# push [20, 40]        push [2, 40]
-# ***pop*** [40]       ***pop*** [40]
-# push [2, 40]         push [8, 40]
-# push [2, 40, 8]      push [2, 40, 8]
-# ***pop*** [8, 40]    ***pop*** [8, 40]
+# (pay 정렬 X)          (pay 정렬 O)           (날짜 정렬 X)
+# push [2]             push [40]             push [2]
+# push [2, 20]         push [20, 40]         push [2, 20]
+# ***pop*** [20]       ***pop*** [40]        ***pop*** [20]
+# push [20, 40]        push [2, 40]          push [20, 40]
+# ***pop*** [40]       ***pop*** [40]        ***pop*** [40]
+# push [2, 40]         push [8, 40]          push [2, 40]
+# push [2, 40, 8]      push [2, 40, 8]       push [2, 40, 8]
+# ***pop*** [8, 40]    ***pop*** [8, 40]     ***pop*** [8, 40]
